e8ec3d6541b4bf36f70c153964d4c24a79b1279f
Dima-salang <luisgabrielle1026@gmail.com>
2026-02-23 21:11:03 +0800

    add: userObserver to create corresponding teacher class per user created

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        if ($user->workos_id) {
+            $user->teacher()->create([
+                'user_id' => $user->workos_id,
+                'full_name' => $user->name,
+            ]);
+        }
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        if ($user->wasChanged('name')) {
+            $user->teacher()->update([
+                'full_name' => $user->name,
+            ]);
+        }
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        if ($user->workos_id) {
+            $user->teacher()->delete();
+        }
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
