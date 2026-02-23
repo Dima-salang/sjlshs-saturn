f5659cc9d973abceef74c207e48114efb924c804
Dima-salang <luisgabrielle1026@gmail.com>
2026-02-23 21:14:41 +0800

    add: api resource for the teachers, sections, and students

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'teacher.active'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
     Route::apiResource('attendance', \App\Http\Controllers\AttendanceController::class)->only(['index', 'store', 'destroy']);
+    Route::apiResource('teachers', \App\Http\Controllers\TeacherController::class)->except(['store']);
+    Route::apiResource('sections', \App\Http\Controllers\SectionController::class);
+    Route::apiResource('students', \App\Http\Controllers\StudentController::class);
     Route::post('qrcodes', [\App\Http\Controllers\QRCodeController::class, 'store'])->name('api.qrcodes.store');
 });
