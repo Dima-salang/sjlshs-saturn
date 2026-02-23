bb69aac0f5b8e08aa6cde2e40d6d2f0ca90be9c1
Dima-salang <luisgabrielle1026@gmail.com>
2026-02-23 21:14:21 +0800

    add: form request and teacher resource

---
+++ app/Http/Requests/Teacher/UpdateTeacherRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Teacher;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTeacherRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'full_name' => ['required', 'string', 'max:255'],
+            'section_advisory' => ['nullable', 'string', 'max:255'],
+            'is_active' => ['required', 'boolean'],
+            'is_admin' => ['required', 'boolean'],
+            'is_nonFaculty' => ['required', 'boolean'],
+        ];
+    }
+}
---
+++ app/Http/Resources/TeacherResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property \App\Models\Teacher $resource
+ */
+class TeacherResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'full_name' => $this->full_name,
+            'section_advisory' => $this->section_advisory,
+            'is_active' => $this->user->is_active ?? false,
+            'is_admin' => $this->user->is_admin ?? false,
+            'is_nonFaculty' => $this->user->is_nonFaculty ?? false,
+            'user' => [
+                'email' => $this->user->email ?? null,
+                'avatar' => $this->user->avatar ?? null,
+            ],
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
